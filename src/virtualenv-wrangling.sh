4caa07c0636f872934c393930364e8b335150b1f
Taavi Päll <taavi74@ut.ee>
2018-03-13 12:17:05 +0200

    virtualenv

--- src/virtualenv-wrangling.sh
+++ src/virtualenv-wrangling.sh
@@ -2,7 +2,7 @@
 
 module load python-3.6.0
 virtualenv env
-source env/bin/activate
-pip install snakemake
-deactivate
+env/bin/pip install snakemake
+env/bin/pip freeze > requirements.txt
 module purge
+
