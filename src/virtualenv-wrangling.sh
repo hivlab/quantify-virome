a94eded8e33c92b2292623ba2a099cd500187b2c
Taavi Päll <taavi74@ut.ee>
2018-03-13 10:57:41 +0200

    create virtual env in project directory

--- src/virtualenv-wrangling.sh
+++ src/virtualenv-wrangling.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 module load python-3.6.0
-virtualenv -p python3 ~/Projects/vs/.venv
-source ~/Projects/vs/.venv/bin/activate
+virtualenv env
+source env/bin/activate
 pip install snakemake
-pip install fontconfig
-
-
-##to deactivate and remove
-cd ~/Projects/vs/
 deactivate
-rm -r ~/Projects/vs/.venv
 module purge
