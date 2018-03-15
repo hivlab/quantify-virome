ca4236f32ac7c1a72061040e029de1e62c3777c2
Taavi Päll <taavi74@ut.ee>
2018-03-15 22:45:29 +0200

    prinseq + cd-hit

--- Snakefile.py
+++ Snakefile.py
@@ -5,7 +5,8 @@ configfile: "/gpfs/hpchome/taavi74/Projects/vs/config.yaml"
 rule all:
     input:
         expand("output/qc/{sample}.stitched.merged_fastqc.html", sample = config["samples"]),
-        expand("output/qc/{sample}.stitched.merged_fastqc.zip", sample = config["samples"])
+        expand("output/qc/{sample}.stitched.merged_fastqc.zip", sample = config["samples"]),
+        expand("output/stitched/{sample}.stitched.merged.prinseq.fasta", sample = config["samples"])
 
 ## Run FastQC -------------------------------------
 
@@ -15,9 +16,51 @@ rule fastqc:
     output:
         html = "output/qc/{sample}.stitched.merged_fastqc.html",
         zip = "output/qc/{sample}.stitched.merged_fastqc.zip"
-    params: ""
     wrapper:
         "0.22.0/bio/fastqc"
+
+
+## Run cd-hit
+
+rule cd-hit:
+  input: 
+    "output/stitched/{sample}.stitched.merged.prinseq.fasta"
+  output: 
+    "output/cdhit/{sample}.stitched.merged.prinseq.QCed.cdhit.fa"
+  params: 
+    ""
+  shell:
+  """
+  cdhit -i {input} -o {output} {params} > {report}
+  """
+
+
+
+## Convert fastq to fasta format
+
+rule fastq2fasta:
+  input: "output/stitched/{sample}.stitched.merged.prinseq.fastq"
+  output: "output/stitched/{sample}.stitched.merged.prinseq.fasta"
+  shell:
+    """
+    sed -n '1~4s/^@/>/p;2~4p' {input} > {output}
+    rm {input}
+    """
+
+## Prinseq
+
+rule prinseq:
+    input: "output/stitched/{sample}.stitched.merged.fq.gz"
+    output: 
+      "output/stitched/{sample}.stitched.merged.prinseq.fastq"
+    params: 
+      settings = "-no_qual_header -min_len 50 -ns_max_p 4 -min_gc 10 -max_gc 90 -derep 14 -lc_method dust -lc_threshold 8 -trim_tail_left 5 -trim_tail_right 5 -trim_ns_left 1 -trim_ns_right 1 -min_qual_mean 25 -trim_qual_left 25 -trim_qual_right 25 -min_qual_score 10",
+      stub = "output/stitched/{sample}.stitched.merged.prinseq"
+    shell:
+      """
+      gzip -dc {input} | prinseq-lite.pl -fastq stdin {params.settings} -out_good {params.stub}
+      rm stdin*
+      """
 
 ## Merge stitched reads --------------------------------------
 
@@ -31,6 +74,7 @@ rule merge_reads:
   shell:
     """
     cat {input.join} {input.un1} {input.un2} > {output.merged}
+    rm {input.join} {input.un1} {input.un2}
     """
 
 ## Stitch paired reads --------------------------------------
