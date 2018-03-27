b09ddb028c488deed8a1e33eb65782ebcbaf1a39
Taavi Päll <taavi74@ut.ee>
2018-03-27 13:57:16 +0300

    split fasta

---
+++ src/split_fasta.py
@@ -0,0 +1,39 @@
+
+
+def batch_iterator(iterator, batch_size):
+    """Returns lists of length batch_size.
+
+    This can be used on any iterator, for example to batch up
+    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+    Alignment objects from Bio.AlignIO.parse(...), or simply
+    lines from a file handle.
+
+    This is a generator function, and it returns lists of the
+    entries from the supplied iterator.  Each list will have
+    batch_size entries, although the final list may be shorter.
+    """
+    entry = True  # Make sure we loop once
+    while entry:
+        batch = []
+        while len(batch) < batch_size:
+            try:
+                entry = iterator.next()
+            except StopIteration:
+                entry = None
+            if entry is None:
+                # End of file
+                break
+            batch.append(entry)
+        if batch:
+            yield batch
+
+
+from Bio import SeqIO
+
+record_iter = SeqIO.parse(snakemake.input[0], "fasta")
+for i, batch in enumerate(batch_iterator(record_iter, snakemake.params["batch_size"])):
+    filename = snakemake.output["stub"] % (i + 1)
+    with open(filename, "w") as handle:
+        count = SeqIO.write(batch, handle, "fasta")
+    print("Wrote %i records to %s" % (count, filename))
+
