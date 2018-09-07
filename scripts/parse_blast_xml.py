9feb2b51f438f41a361c358cae8163d6b928f544
Taavi Päll <taavi74@ut.ee>
2018-09-07 20:47:06 +0300

    write two files on the fly

--- scripts/parse_blast_xml.py
+++ scripts/parse_blast_xml.py
@@ -3,17 +3,16 @@
 # http://biopython.org/DIST/docs/tutorial/Tutorial.html#htoc93
 from Bio import SearchIO
 from Bio import SeqIO
-from helpers import parse_blast
-from helpers import subset_records
 
-# Write known hits to file and return unmapped ids
-blast_results = SearchIO.parse(snakemake.input[0], 'blast-xml')
-keep = parse_blast(blast_results, snakemake.output[0], evalue_threshold = snakemake.params["e_cutoff"])
-keep_lst = list(keep)
+def parse_blast(blast_results, unmapped_masked, known_out_xml, unknown_out_fasta, evalue_threshold = 1e-10):
+  blast_results = SearchIO.parse(blast_results, 'blast-xml')
+  unmapped_masked = SeqIO.index(unmapped_masked, "fasta")
+  with open(known_out_xml, "w") as known_out, open(unknown_out_fasta, "w") as unknown_out:
+    for query in blast_results:
+      if query and query[0][0].evalue < evalue_threshold:
+        SearchIO.write(query, known_out, "blast-xml")
+      else:
+        SeqIO.write(unmapped_masked[query.id], unknown_out, "fasta")
 
-# Subset masked sequences using unmapped_ids
-unmapped_masked = SeqIO.parse(snakemake.input[1], "fasta")
-unmapped_keep = subset_records(unmapped_masked, keep_lst)
-
-# Write unmasked subset to file
-masked_count = SeqIO.write(unmapped_keep, snakemake.output[1], "fasta")
+# Write known hits and unknowns to xml and fasta file, respectively
+parse_blast(snakemake.input[0], snakemake.input[1], snakemake.output[0], snakemake.output[1], evalue_threshold = snakemake.params["e_cutoff"])
