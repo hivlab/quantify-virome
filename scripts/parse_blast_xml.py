511eb9fe12d4a963870b834a4bbddb9f0df9c9e5
Taavi Päll <taavi74@ut.ee>
2018-09-08 13:04:00 +0300

    always generate three files

--- scripts/parse_blast_xml.py
+++ scripts/parse_blast_xml.py
@@ -4,15 +4,24 @@
 from Bio import SearchIO
 from Bio import SeqIO
 
-def parse_blast(blast_results, unmapped_masked, known_out_xml, unknown_out_fasta, evalue_threshold = 1e-10):
-  blast_results = SearchIO.parse(blast_results, 'blast-xml')
-  unmapped_masked = SeqIO.index(unmapped_masked, "fasta")
-  with open(known_out_xml, "w") as known_out, open(unknown_out_fasta, "w") as unknown_out:
+def parse_blast(blast_xml, unknowns_masked_fasta, known_out_xml, known_out_fasta, unknown_out_fasta, evalue_threshold = 1e-10, unknowns_fasta = None):
+  blast_results = SearchIO.parse(blast_xml, 'blast-xml')
+  unmapped_masked = SeqIO.index(unknowns_masked_fasta, "fasta")
+  if unknowns_fasta is not None:
+    unmapped = SeqIO.index(unknowns_fasta, "fasta")
+  with open(known_out_xml, "w") as known_xml, open(known_out_fasta, "w") as known_fa, open(unknown_out_fasta, "w") as unknown_fa:
     for query in blast_results:
       if query and query[0][0].evalue < evalue_threshold:
-        SearchIO.write(query, known_out, "blast-xml")
+        SearchIO.write(query, known_xml, "blast-xml")
+        if unknowns_fasta is not None:
+          SeqIO.write(unmapped[query.id], known_fa, "fasta")
       else:
-        SeqIO.write(unmapped_masked[query.id], unknown_out, "fasta")
+        SeqIO.write(unmapped_masked[query.id], unknown_fa, "fasta")
 
 # Write known hits and unknowns to xml and fasta file, respectively
-parse_blast(snakemake.input[0], snakemake.input[1], snakemake.output[0], snakemake.output[1], evalue_threshold = snakemake.params["e_cutoff"])
+parse_blast(blast_xml = snakemake.input[0],
+            unknowns_masked_fasta = snakemake.input[1],
+            known_out_xml = snakemake.output[0],
+          known_out_fasta = snakemake.output[1],
+        unknown_out_fasta = snakemake.output[2],
+        evalue_threshold = snakemake.params["e_cutoff"])
