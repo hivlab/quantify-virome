05ead5d342fbacc8e6a90f1bc09de66ce0886f93
Taavi Päll <taavi74@ut.ee>
2018-09-17 11:17:12 +0300

    fix input indexing
    libraries to heplers

--- scripts/unmasked_viral.py
+++ scripts/unmasked_viral.py
@@ -1,10 +1,7 @@
 
-from Bio import SearchIO
-from Bio import SeqIO
-import pandas as pd
 from helpers import subset_unmasked_csv
 
 subset_unmasked_csv(blast_csv = snakemake.input[0],
-           unmasked_fasta = snakemake.input[2],
+           unmasked_fasta = snakemake.input[1],
                    output = snakemake.output[0])
 
