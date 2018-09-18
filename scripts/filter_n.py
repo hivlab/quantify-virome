b4681a348cd4d91399850cd11a2e48be3e46973d
Taavi Päll <taavi74@ut.ee>
2018-09-18 15:20:16 +0300

    original two file filter

---
+++ scripts/filter_n.py
@@ -0,0 +1,18 @@
+
+from Bio import SeqIO
+
+def filter_N(masked, original, masked_filt, original_filt, min_length, por_n):
+  """"Filters out sequences with many N-s.
+  Filters out sequeces with less than or equal to min_length non-N bases
+  and sequences with more than por_n % N-s.
+  """"
+  results = SeqIO.parse(masked, 'fasta')
+  original = SeqIO.index(original, "fasta")
+  with open(masked_filt, "w") as masked, open(original_filt, "w") as unmasked:
+    for sequence in results:
+      if ((float(len(sequence)) - float(sequence.count("N"))) >= min_length and
+        (float(sequence.count("N")) / float(len(sequence))) * 100 <= por_n):
+        SeqIO.write(sequence, masked, "fasta")
+        SeqIO.write(original[sequence.id], unmasked, "fasta")
+
+filter_N(snakemake.input[0], snakemake.input[1], snakemake.output[0], snakemake.output[1])
