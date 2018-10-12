2f7be02cedba79e7165c21f0617247940f638b6c
Taavi Päll <tapa741@gmail.com>
2018-10-12 20:24:11 +0300

    out of open

--- scripts/split_fasta.py
+++ scripts/split_fasta.py
@@ -15,6 +15,7 @@ batch_size = ceil(seqs / snakemake.params[0])
 record_iter = SeqIO.parse(snakemake.input[0], "fasta")
 for n, batch in enumerate(batch_iterator(record_iter, batch_size), start = 1):
     if n == snakemake.params[1]:
-        with open(snakemake.output[0], "w") as handle:
+        output = snakemake.output[0]
+        with open(output, "w") as handle:
             count = SeqIO.write(batch, handle, "fasta")
             break
