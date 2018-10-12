bac19635d9bd1aa1d1542bf3f0ddcf0b349ce7e2
Taavi Päll <tapa741@gmail.com>
2018-10-12 21:53:21 +0300

    to integer

--- scripts/split_fasta.py
+++ scripts/split_fasta.py
@@ -15,9 +15,12 @@ batch_size = ceil(seqs / snakemake.params[0])
 # Split sequences into chunks based on batch size and write into files
 record_iter = SeqIO.parse(snakemake.input[0], "fasta")
 
+# wildcard.n
+nth = int(snakemake.params[1])
+
 with open(snakemake.output[0], "w") as handle:
   for n, batch in enumerate(batch_iterator(record_iter, batch_size), start = 1):
-    if n == snakemake.params[1]:
+    if n == nth:
         count = SeqIO.write(batch, handle, "fasta")
-    elif n > snakemake.params[1]:
+    elif n > nth:
         break
