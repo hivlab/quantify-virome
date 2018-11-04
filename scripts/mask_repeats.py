e08509abadb992d30f735059887b24ab8f52df19
Taavi Päll <tapa741@gmail.com>
2018-11-04 11:12:35 +0200

    added messages

--- scripts/mask_repeats.py
+++ scripts/mask_repeats.py
@@ -7,7 +7,7 @@ rm = "RepeatMasker -qq -pa {threads} {input} -dir {outdir}"
 rm = rm.format(threads = str(snakemake.threads), input = snakemake.input["fa"], outdir = snakemake.params["outdir"])
 
 # Run repeatmasker
-p = Popen(rm.split(" "), stdout = PIPE, stderr = PIPE, universal_newlines=True)
+p = Popen(rm.split(" "), stdout = PIPE, stderr = PIPE, universal_newlines = True)
 output, error = p.communicate()
 if p.returncode != 0:
    raise Exception("Job failed\n" + output + error)
@@ -15,5 +15,6 @@ else:
   print(output)
 
 # Filter repeatmasker output
-print("Filtering runs\n")
+print("Filtering runs..\n")
 filters.run_filter_n(snakemake.input, snakemake.output, snakemake.params)
+print("Filtering runs done!\n")
