f202fc8a530efb0ba1dece17712ab100d2486eba
Taavi Päll <tapa741@gmail.com>
2018-11-15 16:34:37 +0200

    updated zenodo upload untested

--- Snakefile
+++ Snakefile
@@ -6,18 +6,20 @@ __license__ = "MIT"
 
 include: "rules/common.smk"
 
+## Main output files
+file_ids = list(range(1, n_files + 1, 1))
+outputs = expand([
+        "results/{sample}_phages_{n}.csv",
+        "results/{sample}_unassigned_{n}.fa",
+        "results/{sample}_phages_blasted_{n}.csv",
+        "results/{sample}_viruses_blasted_{n}.csv"],
+        sample = sample_ids, n = file_ids),
+        expand("taxonomy/{file}.csv", file = ["names", "nodes", "division"])
+
 ## Target rules
 rule all:
     input:
-      expand([
-      "results/{sample}_phages_{n}.csv",
-      "results/{sample}_unassigned_{n}.fa",
-      "results/{sample}_phages_blasted_{n}.csv",
-      "results/{sample}_viruses_blasted_{n}.csv"
-      ],
-      sample = sample_ids,
-      n = list(range(1, n_files + 1, 1))),
-      expand("taxonomy/{file}.csv", file = ["names", "nodes", "division"])
+        outputs, "results/{sample}_phages.csv.tar.gz" if config["zenodo"]["deposition_id"] else outputs
 
 ## Modules
 include: "rules/munge.smk"
--- scripts/zenodo_upload.py
+++ scripts/zenodo_upload.py
@@ -1,29 +1,45 @@
-
 import os
 import requests
-import json
+import gzip
+import re
+from subprocess import Popen
+import hashlib
 
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+# Upload depository id
 deposition_id = snakemake.params[0]
 
-open_file = [open("{}".format(n), "rb") for n in snakemake.input]
-files = list(zip(snakemake.input, open_file))
+# Create tar.gz file for upload
+files = snakemake.input
+zipfile = list(set([re.sub("_\d+", "", file) for file in files]))[0] + ".tar.gz"
+cmd = ['tar', '-cvzf', zipfile] + files
+p = Popen(cmd)
 
-r = requests.get('https://zenodo.org/api/deposit/depositions', params={'access_token': os.environ['ZENODO_PAT']})
-r.status_code
-r.json()
+# Check if file is present
+# calculate md5 checksum for local file
+hash = md5(zipfile)
 
-headers = {"Content-Type": "application/json"}
-r = requests.post('https://zenodo.org/api/deposit/depositions', params={'access_token': os.environ['ZENODO_PAT']}, json={}, headers=headers)
-r.status_code
-r.json()
+# Get info for remote files
+r = requests.get('https://zenodo.org/api/deposit/depositions/{}/files'.format(deposition_id),
+                        params={'access_token': os.environ['ZENODO_PAT']})
+fchk = [{deposit['filename'], deposit['checksum']} for deposit in r.json()]
+filename, checksum = zip(*fchk)
 
-data = {}
-files = {'file': open('results/SRR5580357_phages_1.csv', 'rb')}
-r = requests.post('https://zenodo.org/api/deposit/depositions/{}/files'.format(deposition_id),
-                  params = {'access_token': os.environ['ZENODO_PAT']},
-                  data = data,
-                  files = files)
-print(r.status_code)
-r = requests.post('https://zenodo.org/api/deposit/depositions/{}/actions/publish'.format(deposition_id),
-                      params={'access_token': os.environ['ZENODO_PAT']})
-print(r.status_code)
+if hash not in list(checksum):
+    with open(zipfile, "rb") as handle:
+        r = requests.post('https://zenodo.org/api/deposit/depositions/{}/files'.format(deposition_id),
+                          params = {'access_token': os.environ['ZENODO_PAT']},
+                          data = {'filename': str(zipfile)},
+                          files = {'file': handle})
+
+        if r.status_code != 201:
+                raise requests.HTTPError(f"Error in data upload, status code: {r.status_code}   {r.json()['message']}")
+
+else:
+    print("File {} is already uploaded as {}".format(zipfile, [f for f in list(filename) if f == os.path.basename(list(filename))][0]))
