d44ae14f137c94736e2732af887fb8544f08feeb
Taavi Päll <tapa741@gmail.com>
2019-09-19 11:09:20 +0300

    removed uncultured accessions script

--- rules/get_uncultured_taxids.py
+++
@@ -1,8 +0,0 @@
-from Bio import Entrez
-
-Entrez.email = {snakemake.params["email"]}
-resp = Entrez.esearch(db = "taxonomy", term = '"environmental samples"[organism] OR metagenomes[orgn]')
-cont = Entrez.read(resp)
-with open(snakemake.output[0], "w") as f:
-    for item in cont["IdList"]:
-        f.write("{}\n".format(item))
